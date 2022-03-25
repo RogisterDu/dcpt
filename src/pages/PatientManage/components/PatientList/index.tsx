c1ab2dc037186bee63a3205305940360947e7aa7
Rogister <rogisterzs@gmail.com>
2022-03-25 12:56:22 +0800

    feat: 详情组件

--- src/pages/PatientManage/components/PatientList/index.tsx
+++ src/pages/PatientManage/components/PatientList/index.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { List, message, Avatar } from 'antd';
 import VirtualList from 'rc-virtual-list';
 import styles from '../../index.less';
-// interface Pprops {
-// }
-const PatientList: React.FC = () => {
+interface Pprops {
+  handleId: any;
+}
+const PatientList: React.FC<Pprops> = ({ handleId }) => {
   const fakeDataUrl =
     'https://randomuser.me/api/?results=20&inc=name,gender,email,nat,picture&noinfo';
-  const ContainerHeight = 800;
+  const ContainerHeight = 900;
   const [data, setData] = useState([]);
 
   const appendData = () => {
@@ -27,17 +28,22 @@ const PatientList: React.FC = () => {
       appendData();
     }
   };
+
+  const itemCLick = (Id: string) => {
+    handleId(Id);
+  };
+
   return (
     <List className={`${styles.patientList}`}>
       <VirtualList
         data={data}
         height={ContainerHeight}
-        itemHeight={47}
+        itemHeight={20}
         itemKey="email"
         onScroll={onScroll}
       >
         {(Pitem: any) => (
-          <List.Item key={Pitem.email}>
+          <List.Item key={Pitem.email} onClick={() => itemCLick(Pitem.Id)}>
             <List.Item.Meta
               avatar={<Avatar src={Pitem.picture.large} />}
               title={<a href="https://ant.design">{Pitem.name.last}</a>}
--- src/pages/PatientManage/index.tsx
+++ src/pages/PatientManage/index.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PatientList from './components/PatientList';
+import { Tabs } from 'antd';
 import styles from './index.less';
 
+const { TabPane } = Tabs;
+
 const PatientManage: React.FC = () => {
+  const [patientId, setPatientId] = useState('');
+  const [tabkey, setTabkey] = useState('1');
+  const changePatientId = (Id: string) => {
+    setPatientId(Id || '');
+  };
+
+  function Tabscallback(key: any) {
+    // console.log(key);
+    setTabkey(key);
+  }
+  useEffect(() => {}, [patientId, tabkey]);
+
+  //空白页面渲染
+  const renderBlank = () => {
+    return <div className={styles.blank}>暂无数据</div>;
+  };
+
+  //Tabs渲染
+  const renderTabs = () => {
+    return (
+      <Tabs defaultActiveKey="1" animated type="card" onChange={Tabscallback}>
+        <TabPane tab="个人信息" key="1">
+          <div className={`${styles.tabContent}`}>Content of Tab Pane 1</div>
+        </TabPane>
+        <TabPane tab="电子病例" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="收费记录" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+        <TabPane tab="医学影像" key="4">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
+    );
+  };
   return (
     <div className={`${styles.flexRow}`}>
       <div className={`${styles.leftArea}`}>
         <h1>病人列表</h1>
-        <PatientList />
+        <PatientList handleId={changePatientId} />
       </div>
-      <div className="{`${styles.rightArea}`}">病人信息</div>
+      <div className="{`${styles.rightArea}`}">{!patientId ? renderTabs() : renderBlank()}</div>
     </div>
   );
 };
