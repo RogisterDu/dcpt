533100e4a40520fd3e1651ae5bdbde3e109eb670
Rogister <rogisterzs@gmail.com>
2022-03-29 14:35:45 +0800

    feat: 电子病历style

--- src/pages/MedicalRecord/components/RecordCard/index.tsx
+++ src/pages/MedicalRecord/components/RecordCard/index.tsx
@@ -1,38 +1,73 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Descriptions, Modal, Form } from 'antd';
 import styles from '../../index.less';
+import '../../antdCover.less';
 import { AppstoreAddOutlined } from '@ant-design/icons';
 interface RecordCardProps {
   recordItem: any;
 }
 
 interface infoInterface {
-  title: string;
-  content: string;
-  recordTime: string;
+  main: string;
+  now: string;
+  cure: string;
+  location: string;
   history: string;
+  allergic: string;
 }
 
 const RecordCard: React.FC<RecordCardProps> = ({ recordItem }) => {
+  const [editVisable, setEditVisable] = React.useState(false);
   const { time, info } = recordItem;
+
+  const [editForm] = Form.useForm();
+
   const rendertitle = () => {
     return <div className={styles.time}>{time}</div>;
   };
   const renderInfo = (Detailinfo: infoInterface) => {
     console.log('Detailinfo', Detailinfo);
+    const { main, now, cure, location, history, allergic } = Detailinfo;
     return (
       <div className={styles.details}>
-        <div className={styles.title}>{Detailinfo.title || ''}</div>
-        <div className={styles.content}>{Detailinfo.content}</div>
-        <div className={styles.history}>{Detailinfo.history}</div>
+        <Descriptions bordered>
+          <Descriptions.Item label="主诉" span={4}>
+            {main || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="现病史" span={2}>
+            {now || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="既往史" span={2}>
+            {history || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="流行病史" span={2}>
+            流行病史
+          </Descriptions.Item>
+          <Descriptions.Item label="过敏史" span={4}>
+            {allergic || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="检查" span={4}>
+            {location || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="治疗方案" span={4}>
+            {cure || '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="医嘱" span={4}>
+            医嘱
+          </Descriptions.Item>
+        </Descriptions>
       </div>
     );
   };
 
+  const toAddNewDetail = () => {
+    setEditVisable(true);
+  };
+
   const renderAddDetail = () => {
     return (
-      <div className={styles.flexcenter}>
-        <a>
+      <div className={`${styles.flexcenter} ${styles.detailsArea}`}>
+        <a onClick={() => toAddNewDetail}>
           <AppstoreAddOutlined />
           &nbsp;&nbsp;添加诊断
         </a>
@@ -40,13 +75,44 @@ const RecordCard: React.FC<RecordCardProps> = ({ recordItem }) => {
     );
   };
 
+  const submitEdit = () => {
+    setEditVisable(false);
+  };
+
+  const toEdit = (id: any) => {
+    console.log('id', id);
+    setEditVisable(true);
+  };
+
+  const renderCardExtra = (id: any) => {
+    console.log('id', id);
+    return <a onClick={() => toEdit(id)}>编辑</a>;
+  };
+
   return (
     <>
+      <Modal
+        destroyOnClose
+        visible={editVisable}
+        closable
+        onOk={() => submitEdit}
+        onCancel={() => {
+          setEditVisable(false);
+        }}
+      >
+        <Form form={editForm}>
+          <Form.Item label="诊断">
+            <input />
+          </Form.Item>
+        </Form>
+      </Modal>
       <Card
+        hoverable
         key={recordItem.id}
         title={rendertitle()}
-        extra={<a href="#">操作</a>}
+        extra={info && Object.keys(info).length > 0 ? renderCardExtra(recordItem.id) : null}
         style={{ width: 800 }}
+        className="cardCover"
       >
         {info && Object.keys(info).length > 0 ? renderInfo(info) : renderAddDetail()}
       </Card>
--- src/pages/MedicalRecord/index.tsx
+++ src/pages/MedicalRecord/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Timeline } from 'antd';
 import RecordCard from './components/RecordCard';
-// import styles from './index.less';
+import styles from './index.less';
+import './antdCover.less';
 // import { AppstoreAddOutlined } from '@ant-design/icons';
 
 interface MedicalRecordProps {
@@ -9,10 +10,14 @@ interface MedicalRecordProps {
 }
 
 const MedicalRecord: React.FC<MedicalRecordProps> = ({ medicalRecord }) => {
+  // const handletoEdit = (id: any) => {
+  //   console.log('id', id);
+  // };
+
   const renderTimeLines = (recordItem: any) => {
     console.log('1111', recordItem);
     return (
-      <Timeline.Item>
+      <Timeline.Item label={recordItem.time}>
         <RecordCard recordItem={recordItem} />
       </Timeline.Item>
     );
@@ -21,11 +26,17 @@ const MedicalRecord: React.FC<MedicalRecordProps> = ({ medicalRecord }) => {
     {
       id: 1,
       time: '2020-01-01',
+      isFirst: 0,
+      doctor: '无',
       info: {
         title: '这是病例记录',
-        content: '这是病例记录',
-        // recordTime: '2020-01-01',
+        // content: '这是病例记录',
         history: '123',
+        allergic: '无',
+        main: '牙疼',
+        now: '无',
+        cure: '治疗方案',
+        location: '无',
       },
     },
     {
@@ -36,8 +47,8 @@ const MedicalRecord: React.FC<MedicalRecordProps> = ({ medicalRecord }) => {
   ];
 
   return (
-    <>
-      <Timeline>
+    <div className={styles.medicalArea}>
+      <Timeline mode="left">
         {medicalRecord?.map((recordItem: any) => {
           renderTimeLines(recordItem);
         }) || null}
@@ -45,7 +56,7 @@ const MedicalRecord: React.FC<MedicalRecordProps> = ({ medicalRecord }) => {
           return renderTimeLines(recordItem);
         }) || null}
       </Timeline>
-    </>
+    </div>
   );
 };
 export default MedicalRecord;
